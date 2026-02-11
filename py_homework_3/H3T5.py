91889cfff22812aa372ad11087714ea6d6a0247d
Stepan-Khramov <sakhramov@gmail.com>
2026-02-11 14:04:52 +0300

    HW H3T5 in 3 variants

--- py_homework_3/H3T5.py
+++ py_homework_3/H3T5.py
@@ -31,20 +31,31 @@ def main():
     #     for row in formated_dates: 
     #         print(row)
 
+    # found_dates = []
+    # formated_dates = ''
+    # txt = str(input())
+    # found_dates_tmp = re.findall(r'((0[1-9]|[12][0-9]|3[01]|[12][0-9]{3})[/\-.](0[1-9]|1[0-2])[/\-.]([12][0-9]{3}|0[1-9]|[12][0-9]|3[01]))', txt)
+    # if len(found_dates_tmp) == 0:
+    #     print('В тексте дат нет')
+    # else:
+    #     for i in range(len(found_dates_tmp)):
+    #         found_dates.append(found_dates_tmp[i][0]) 
+    #     tmp_formated_dates = re.sub(r'(\d{2,4})[/\-.](\d{2})[/\-.](\d{2,4})', r"\3.\2.\1", ' '.join(found_dates))
+    #     formated_dates = (re.sub(r'(\d{4})\.(\d{2})\.(\d{2})', r"\3.\2.\1", tmp_formated_dates)).split()
+    #     for row in formated_dates: 
+    #         print(row)
+
     found_dates = []
     formated_dates = ''
     txt = str(input())
-    found_dates_tmp = re.findall(r'((0[1-9]|[12][0-9]|3[01]|[12][0-9]{3})[/\-.](0[1-9]|1[0-2])[/\-.]([12][0-9]{3}|0[1-9]|[12][0-9]|3[01]))', txt)
-    if len(found_dates_tmp) == 0:
+    found_dates = re.findall(r'\b(?:(?:0?[1-9]|[12]\d|3[01])[-/.](?:0?[1-9]|1[0-2])[-/.](?:\d{4}|\d{2})|(?:\d{4}|\d{2})[-/.](?:0?[1-9]|1[0-2])[-/.](?:0?[1-9]|[12]\d|3[01]))\b', txt)
+    if len(found_dates) == 0:
         print('В тексте дат нет')
     else:
-        for i in range(len(found_dates_tmp)):
-            found_dates.append(found_dates_tmp[i][0]) 
         tmp_formated_dates = re.sub(r'(\d{2,4})[/\-.](\d{2})[/\-.](\d{2,4})', r"\3.\2.\1", ' '.join(found_dates))
         formated_dates = (re.sub(r'(\d{4})\.(\d{2})\.(\d{2})', r"\3.\2.\1", tmp_formated_dates)).split()
         for row in formated_dates: 
             print(row)
-
 
     print("Script execution finished.")
     return 0
