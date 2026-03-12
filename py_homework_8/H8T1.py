ad0f862891be285a3bb4bcd5166d49a3909f5ef0
Stepan-Khramov <sakhramov@gmail.com>
2026-03-12 17:00:19 +0300

    H8T1 done

---
+++ py_homework_8/H8T1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Brief description of the script and its purpose.
+
+This module does X and Y.
+
+Example:
+    $ python your_script_name.py
+"""
+
+import sys
+# Import other standard library or third-party packages here
+import pandas as pd
+
+
+def main():
+    """Main function of the script."""
+    print("Script execution started.")
+    
+#=========================================================================================
+# Homework code. Begin.
+#=========================================================================================
+
+    df = pd.read_csv('athlete_events.csv')
+
+    fra_2008 = len(df[(df.Year == 2008) & (df.NOC == 'FRA')]['Name'].unique().tolist())
+    print(fra_2008)
+#=========================================================================================
+# Homework code. End.
+#=========================================================================================
+
+    print("Script execution finished.")
+    return 0
+
+if __name__ == "__main__":
+    # This block allows the script to be run directly from the command line
+    # or imported as a module without executing the main function automatically.
+    # sys.exit() is used to ensure the script exits with the status code
+    # returned by the main function.
+    sys.exit(main())
